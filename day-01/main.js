94a40240d1fcf4aa99aec8c38112c81055b6eb41
Jukka Pajarinen <jukka.pajarinen@lumitum.com>
2022-12-18 20:12:12 +0200

    Solution for day 1 part 2

--- day-01/main.js
+++ day-01/main.js
@@ -5,15 +5,18 @@ const path = process.argv[1].replace("/main.js", "");
 const buffer = fs.readFileSync(`${path}/input.txt`);
 const rows = buffer.toString().trimEnd().split("\n");
 
-let [bestLoad, curLoad] = [0, 0];
+let [loads, curLoad] = [[], 0];
 rows.forEach(row => {
   if (row === "") {
-    bestLoad = curLoad > bestLoad ? curLoad : bestLoad;
+    loads.push(curLoad);
     curLoad = 0;
   } else {
     curLoad += Number(row);
   }
 });
 
+bestLoad = loads.sort((a, b) => b - a)[0];
+threeBestLoads = loads.sort((a, b) => b - a).slice(0, 3).reduce((a, b) => a + b, 0)
+
 console.log(`Best Elf's Load: ${bestLoad} (part 1)`);
-console.log(`Another something: ${2} (part 2)`);
+console.log(`Best 3 Elf's loads: ${threeBestLoads} (part 2)`);
