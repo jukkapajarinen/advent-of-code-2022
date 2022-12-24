2ccb850997b0bcc866b8f2281e5408ea248bbe06
Jukka Pajarinen <jukka.pajarinen@lumitum.com>
2022-12-24 21:36:53 +0200

    Solution to day 4 part 2

--- day-04/main.js
+++ day-04/main.js
@@ -7,13 +7,15 @@ const rows = buffer.toString().trimEnd().split("\n");
 
 const range = (start, stop, step) => Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step);
 let fullIntersections = 0;
+let partialIntersections = 0;
 
 rows.forEach(row => {
   let bounds1 = row.split(",")[0].split("-").map(x => Number(x));
   let bounds2 = row.split(",")[1].split("-").map(x => Number(x));
   let [left, right] = [range(bounds1[0], bounds1[1], 1), range(bounds2[0], bounds2[1], 1)];
   fullIntersections += left.every(l => right.includes(l)) || right.every(r => left.includes(r)) ? 1 : 0;
+  partialIntersections += left.some(l => right.includes(l)) || right.some(r => left.includes(r)) ? 1 : 0;
 });
 
 console.log(`Full intersecitons: ${fullIntersections} (part 1)`);
-console.log(`Another something: ${2} (part 2)`);
+console.log(`Partial Intersections: ${partialIntersections} (part 2)`);
